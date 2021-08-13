4b8f8483d325871ed425c49985923b801c6edbee
nineya <361654768@qq.com>
2021-08-13 13:38:57 +0800

    fix(btoc):修复多级嵌套时目录为空bug

--- btoc/js/btoc.js
+++ btoc/js/btoc.js
@@ -28,7 +28,7 @@ function Btoc(tocList, contentElement, tocElement, scrollSpeed, headingsOffset){
             if(tocList.indexOf(elem.tagName) != -1){
                 childs.push(elem);
             }
-            childs.concat(getChild(elem, tocList));
+            childs.push.apply(childs, getChild(elem, tocList));
         }
         return childs;
     }
